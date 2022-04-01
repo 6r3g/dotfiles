c620ae62e6d4942bebea824d9e31c388c6f8c4af
kali <kali@kali-dev.local>
2022-04-01 20:32:44 +0200

    xx

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo apt install -y i3-gaps i3-gaps-wm compton rofi papirus-icon-theme flameshot feh 
-sudo apt install -y code-oss remmina zathura unclutter imagemagick htop neofetch arandr
+sudo apt install -y i3-gaps i3-gaps-wm i3blocks i3status compton rofi papirus-icon-theme feh lxappearance
+sudo apt install -y code-oss remmina zathura unclutter imagemagick htop neofetch arandr flameshot
 
 # Fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/UbuntuMono.zip
@@ -16,21 +16,13 @@ sudo apt install -f
 # GTK Nordic theme
 wget https://github.com/EliverLara/Nordic/archive/refs/tags/v2.1.0.zip
 sudo unzip v2.1.0.zip -d /usr/share/themes/
-mv ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml.old
-cp xsettings.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
+lxappearance && sleep 30
 
 # Papirus Nord 
 git clone https://github.com/Adapta-Projects/Papirus-Nord.git
 cd Papirus-Nord
-
-PREFIX="Icons"
-LOCAL="/usr/share/icons/Papirus"
-sudo cp -f $PREFIX/64x64/* $LOCAL/64x64/places
-sudo cp -f $PREFIX/48x48/* $LOCAL/48x48/places
-sudo cp -f $PREFIX/32x32/* $LOCAL/32x32/places
-sudo cp -f $PREFIX/24x24/* $LOCAL/24x24/places
-sudo cp -f $PREFIX/22x22/* $LOCAL/22x22/places
-cp -f papirus-folders /usr/bin
+chmod u+x install
+Y | ./install
 /usr/bin/papirus-folders -C frostblue3
 cd ..
 
@@ -39,15 +31,18 @@ mkdir -p ~/.config/compton
 mkdir -p ~/.config/rofi
 mkdir -p ~/.config/alacritty
 mkdir -p ~/.wallpaper
-cp -r i3/ ~/.config/i3/
+cp -r i3/ ~/.config/
 cp alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 cp compton/compton.conf ~/.config/compton/compton.conf
-cp -r rofi/ ~/.config/rofi/
+cp -r rofi/ ~/.config/
+find ~/.config/rofi/ -type f -iname "*.sh" -exec chmod +x {} \;
 cp fehbg ~/.fehbg
-cp -r wallpaper ~/.wallpaper 
+chmod u+x ~/.fehbg
+cp wallpaper/wallpaper.jpg ~/.wallpaper/
 
 # change default terminal ( => qterminal to alacritty)
 sudo update-alternatives --config x-terminal-emulator 
 
 # OhMyZsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cd ..
