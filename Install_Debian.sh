47c53ad7e68422eb6a76cf6eba23141a6140ee10
6r3g <6r3g@protonmail.com>
2024-07-27 09:46:57 +0200

    remove newgrp

--- Install_Debian.sh
+++ Install_Debian.sh
@@ -122,7 +122,6 @@ echo \
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker $user
-newgrp docker
 
 # Install exegol
 pipx install exegol
